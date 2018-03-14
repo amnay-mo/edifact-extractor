a6520844062b2fecc7af05368b9456df9a37bcc0
karde <kardelen.karatas@gmail.com>
2018-03-14 15:02:47 +0100

    sql DDL file

---
+++ create.sql
@@ -0,0 +1,54 @@
+CREATE TABLE LOCATION (
+	loc_id varchar NOT NULL,
+	loc_type varchar,
+	loc_name varchar,
+	longitude FLOAT,
+	latitude FLOAT,
+	start_date DATE,
+	end_date DATE,
+	country varchar NOT NULL,
+	time_zone varchar,
+	info varchar,
+	CONSTRAINT LOCATION_pk PRIMARY KEY (loc_id)
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE TRAIN (
+	train_id varchar NOT NULL,
+	company_1_id varchar NOT NULL,
+    company_2_id varchar,
+	ser_character varchar,
+	pricing_cat integer,
+	item_des integer,
+	start_date DATE,
+	end_date DATE,
+	period_days varchar NOT NULL,
+	train_type varchar,
+	CONSTRAINT TRAIN_pk PRIMARY KEY (train_id,company_1_id,period_days,start_date,end_date)
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE TIMETABLE (
+	train_id BINARY NOT NULL,
+	loc_id BINARY NOT NULL,
+	arrival_time TIME,
+	departure_time TIME,
+	itinerary integer NOT NULL,
+	id serial NOT NULL,
+	CONSTRAINT TIMETABLE_pk PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE TIMETABLE ADD CONSTRAINT TIMETABLE_fk0 FOREIGN KEY (train_id) REFERENCES TRAIN(train_id);
+ALTER TABLE TIMETABLE ADD CONSTRAINT TIMETABLE_fk1 FOREIGN KEY (loc_id) REFERENCES LOCATION(loc_id);
